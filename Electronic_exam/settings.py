139eda5f9a34a9620d62de71edfc84dc1c28b69d
chaindmhl <117880660+chaindmhl@users.noreply.github.com>
2026-01-30 15:48:29 +0800

    Update settings.py

--- Electronic_exam/settings.py
+++ Electronic_exam/settings.py
@@ -65,16 +65,17 @@ WSGI_APPLICATION = 'Electronic_exam.wsgi.application'
 # --------------------------
 # DATABASE
 # --------------------------
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("POSTGRES_DB"),
-        'USER': os.environ.get("POSTGRES_USER"),
-        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
-        'HOST': os.environ.get("POSTGRES_HOST"),
-        'PORT': os.environ.get("POSTGRES_PORT", "5432"),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get("POSTGRES_DB"),
+#         'USER': os.environ.get("POSTGRES_USER"),
+#         'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+#         'HOST': os.environ.get("POSTGRES_HOST"),
+#         'PORT': os.environ.get("POSTGRES_PORT", "5432"),
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
