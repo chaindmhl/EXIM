fa00dbd4934cb4e4dd1b7e58b94080583ed105da
chaindmhl <117880660+chaindmhl@users.noreply.github.com>
2026-01-30 16:16:53 +0800

    Update models.py

--- board_exam/models.py
+++ board_exam/models.py
@@ -11,7 +11,10 @@ import os, django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Electronic_exam.settings")
 django.setup()
 
-
+# check/create superuser safely
+User = get_user_model()
+if not User.objects.filter(is_superuser=True).exists():
+    User.objects.create_superuser("admin", "admin@example.com", "admin@03")
 
 def get_board_exam_choices():
     return [(exam, exam) for exam in BOARD_EXAM_TOPICS.keys()]
