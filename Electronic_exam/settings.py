d843e43d35e0e96587497be03d3ed3c5ebce1215
chaindmhl <117880660+chaindmhl@users.noreply.github.com>
2026-01-30 15:57:00 +0800

    Update settings.py

--- Electronic_exam/settings.py
+++ Electronic_exam/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME', 'electronic_board'),
         'USER': os.environ.get('DB_USER', 'admin'),
         'PASSWORD': os.environ.get('DB_PASSWORD', 'admin@03'),
-        'HOST': os.environ.get('DB_HOST', '/cloudsql/concrete-potion-477505-p2:asia-southeast1:exim-db'),
+        'HOST': f'/cloudsql/{os.environ.get("DB_HOST")}',
         'PORT': '5432',
     }
 }
