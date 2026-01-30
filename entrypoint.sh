1c56d3ac0c4b897e76415b53a21aaa8afeca4dd1
chaindmhl <117880660+chaindmhl@users.noreply.github.com>
2026-01-30 11:27:45 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
+
 # Fail fast if SECRET_KEY is not set
 if [ -z "$DJANGO_SECRET_KEY" ]; then
   echo "ERROR: DJANGO_SECRET_KEY is not set"
   exit 1
 fi
 
-# Collect static files at runtime (with runtime env vars)
+# Run collectstatic at runtime (inside container)
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 # Start Gunicorn
+echo "Starting Gunicorn..."
 exec gunicorn Electronic_exam.wsgi:application --bind 0.0.0.0:8080
-
