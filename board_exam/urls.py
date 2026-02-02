0b21c8edacd7701fa92f8a457380aa2f46a04181
chaindmhl <117880660+chaindmhl@users.noreply.github.com>
2026-02-02 08:35:53 +0800

    Update urls.py

--- board_exam/urls.py
+++ board_exam/urls.py
@@ -11,7 +11,7 @@ router = DefaultRouter()
 
 urlpatterns = [
     # Root goes to home
-    path('', views.login, name='login'),
+    path('', views.home, name='home'),
 
     # Authentication / user
     path('signup/', views.signup, name='signup'),
